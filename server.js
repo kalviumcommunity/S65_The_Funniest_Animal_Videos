eb0c503411815410e7f7afdbf3437f6669218cdb
dinesh <shrutidinesh1124@gmail.com>
2025-02-14 10:13:15 +0530

    render deployment

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 
+
+app.use(express.json())
 const port = process.env.PORT || 5000
+
+app.get('/ping', (req,res) => {
+    res.send('pong')
+})
 
 app.listen(port, function() {
     console.log(`Server is running on port ${port}`)
